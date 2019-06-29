4028ed5c74c5fb60585cb531e25ba884c6bae0b9
Eugene Tyurin <eugene-ii@mail.ru>
2019-06-29 21:27:21 +0300

    1.3. JavaDoc. Code convention[#154776]

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,5 +1,13 @@
 package main.java.ru.job4j.calculate;
+
+/**
+* класс Calculate
+*/
 public class Calculate {   
+	/**
+	* main method
+	* @param args
+	*/
    public static void main(String[] args) {
       System.out.println("Hello, Job4j.");
    }
---
+++ chapter_001/src/main/java/ru/job4j/calculate/package-info.java
@@ -0,0 +1,8 @@
+/**
+* Package for calculate task.
+*
+* @author Eugene (eugene-ii@mail.ru)
+* @version $Id$
+* @since 0.1
+*/
+package main.java.ru.job4j.calculate;
