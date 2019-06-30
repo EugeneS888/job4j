d4d0d6b1b33d7dde05d6efda578c21aebacd0f75
Eugene Tyurin <eugene-ii@mail.ru>
2019-06-30 10:08:57 +0300

    Обновление списка команд git/mvn/pom в git_command.txt

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -8,7 +8,16 @@ public class Calculate {
 	* main method
 	* @param args Массив строковых данных
 	*/
-   public static void main(String[] args) {
-      System.out.println("Hello, Job4j.");
-   }
+    public static void main(String[] args) {
+	  System.out.println("Hello, Job4j.");
+    }
+   
+	/**
+	 * Method echo.
+	 * @param name Your name.
+	 * @return Echo plus your name.
+	 */
+	 public String echo(String name) {
+		return "Echo, echo, echo : " + name;
+	 }
 }
