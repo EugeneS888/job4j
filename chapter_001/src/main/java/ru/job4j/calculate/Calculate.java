cf9755946b1209a1dc800c60eba122300ca73c34
Eugene Tyurin <eugene-ii@mail.ru>
2019-06-30 12:45:11 +0300

    Импорт Maven проекта.[#154723]

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,23 +1,25 @@
 package ru.job4j.calculate;
 
 /**
-* класс Calculate
-*/
-public class Calculate {   
-	/**
-	* main method
-	* @param args Массив строковых данных
-	*/
+ * класс Calculate
+ */
+public class Calculate {
+    /**
+     * main method
+     *
+     * @param args Массив строковых данных
+     */
     public static void main(String[] args) {
-	  System.out.println("Hello, Job4j.");
+        System.out.println("Hello, Job4j.");
     }
-   
-	/**
-	 * Method echo.
-	 * @param name Your name.
-	 * @return Echo plus your name.
-	 */
-	 public String echo(String name) {
-		return "Echo, echo, echo : " + name;
-	 }
+
+    /**
+     * Method echo.
+     *
+     * @param name Your name.
+     * @return Echo plus your name.
+     */
+    public String echo(String name) {
+        return "Echo, echo, echo : " + name;
+    }
 }
