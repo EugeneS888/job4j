9ad0c81941484800698cdf8fac99dc4b866b3e87
Eugene Tyurin <eugene-ii@mail.ru>
2019-07-01 21:48:40 +0300

    3.1. Элементарный калькулятор.[#154766] Доблены javadoc теги

--- chapter_001/src/main/java/ru/job4j/calculator/Calculator.java
+++ chapter_001/src/main/java/ru/job4j/calculator/Calculator.java
@@ -1,15 +1,51 @@
 package ru.job4j.calculator;
 
+/**
+ * Простой калькулятор.
+ *
+ * @author Eugene
+ */
 public class Calculator {
+    /**
+     * Сложение двух чисел.
+     *
+     * @param first  первое
+     * @param second второе
+     * @return сумма
+     */
     public double add(double first, double second) {
         return first + second;
     }
+
+    /**
+     * Разность двух чисел.
+     *
+     * @param first  первое
+     * @param second второе
+     * @return разность
+     */
     public double subtract(double first, double second) {
         return first - second;
     }
+
+    /**
+     * Деление двух чисел.
+     *
+     * @param first  первое
+     * @param second второе
+     * @return частное
+     */
     public double div(double first, double second) {
         return first / second;
     }
+
+    /**
+     * Умножение двух чисел
+     *
+     * @param first  первое
+     * @param second второе
+     * @return произведение
+     */
     public double multiple(double first, double second) {
         return first * second;
     }
